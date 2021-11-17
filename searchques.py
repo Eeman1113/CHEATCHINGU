95310b8681a56d4d826698f713d592576321f3b7
Eeman Majumder <54275491+Eeman1113@users.noreply.github.com>
2021-11-17 11:17:27 +0530

    update

--- searchques.py
+++ searchques.py
@@ -1,5 +1,4 @@
 import  google 
 import  wikipedia 
-
-''' funtion to take a text file named recognised.txt and goggle all the sentences from it and put the results in a new text file named ans.tx'''
+import os
 
