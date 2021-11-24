d931917309e62fd78b3bc018d7cc29034df945e1
Eeman Majumder <54275491+Eeman1113@users.noreply.github.com>
2021-11-24 19:21:49 +0530

    search optimised

--- searchques.py
+++ searchques.py
@@ -2,3 +2,20 @@ import  google
 import  wikipedia 
 import os
 
+def question_extracter(a):
+    l=[]
+    for i in a:
+        print(i)
+        if i=='Q':
+            for j in a[a.index(i):]:
+                if j=='?':
+                    l.append(j)
+                    print(l)
+                    break
+        else:
+            continue
+                
+a=input("Enter the question: ")
+a=list(a)
+print(a)
+question_extracter(a)
