945e14b141c2c96b2d2b9a838cdc6c36e0912c2f
Eeman Majumder <54275491+Eeman1113@users.noreply.github.com>
2021-11-26 18:22:56 +0530

    haha intigrated question sorter

--- bashing_my_head_against_the_wall.py
+++ bashing_my_head_against_the_wall.py
@@ -2,14 +2,24 @@ import  google
 import  wikipedia 
 import os
 
-def question_extracter(a):
+def question_extracter():
+    with open('recognized.txt','r') as g:
+        b=g.read()
+        '''remove empty strings from b'''
+        b=list(filter(None,b.split('\n')))#remove empty strings from b
+        
+
+            
+    
+    
+    b=list(b)
     l=[]
     t=[]
-    f=len(a)
-    for i in range(0,f):#loop running for length of a
+    f=len(b)
+    for i in range(0,f):#loop running for length of b
         
-        if a[i]=='Q':#if statement to find weather index value is q or not 
-            for j in a[i:]:#loop running for all elements of a from index of Q to end
+        if b[i]=='Q':#if statement to find weather index value is q or not 
+            for j in b[i:]:#loop running for all elements of b from index of Q to end
                 if j=='?':#breaking loop if a ? is found
                     l.append(j)#appending ? to l
                     t.append(l)#appending l to t for making a list with all the questions
@@ -20,7 +30,4 @@ def question_extracter(a):
         else:# if a q is not found then continue the loop
             continue
     print(t)#the list with all the questions is printed
-a=input("Enter the question: ")
-a=list(a)
-print(a)
-question_extracter(a)
+question_extracter()
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import screenshot
 import img_to_text
 import half_slicer
+import searchques
 
 
 screenshot.screenshot()
 half_slicer.half_slicer()
 img_to_text.img_to_text()
+searchques.searchques()
